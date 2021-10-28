b1e2b3f3cf220265110ca9c4e0da381443b26393
bayuajinurmnsh <bayuaji.nurmansah16@gmail.com>
2021-10-28 19:31:50 +0700

    add Template-driven form validation

--- TodoApp2/src/app/componenst/add-todo-form/add-todo-form.component.ts
+++ TodoApp2/src/app/componenst/add-todo-form/add-todo-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Todo } from '../../models/Todo';
 
 @Component({
@@ -10,22 +11,19 @@ export class AddTodoFormComponent {
 
   @Output() newTodoEvent = new EventEmitter<Todo>();
 
-  
-
   inputTodo:string ="";
+
+  valid: any[] = [];
 
   addTodo(){
     const todo: Todo={
       content:this.inputTodo,
       completed:false
     };
-
     this.newTodoEvent.emit(todo)
-    
     this.inputTodo ="";
 
-      
+   
   }
-  
 
 }
